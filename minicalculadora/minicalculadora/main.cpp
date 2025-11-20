bb6a41f8c9d8a2c02fcee9f5c44df61c73165150
AzureAD\RubenAlvarezGarcia <ruben.alvarez@estudiant.enti.cat>
2025-11-20 09:44:21 +0100

    suma y resta

--- minicalculadora/minicalculadora/main.cpp
+++ minicalculadora/minicalculadora/main.cpp
@@ -5,9 +5,27 @@ int main() {
 
 	double a;
 	double b;
+	short opcion;
 	std::cout << "introduce num 1:" << std::endl;
 	std::cin >> a;
 	std::cout << "introduce num 2:" << std::endl;
 	std::cin >> b;
+	std::cout << "Que quieres hacer sumar(1) restar(2)" << std::endl;
+	std::cin >> opcion;
 
+	switch (opcion)
+	{
+	case 1:
+		std::cout << suma(a, b) << std::endl;
+		break;
+	case 2:
+		std::cout << resta(a, b) << std::endl;
+		break;
+	default:
+		std::cout << "Opción no valida" << std::endl;
+		break;
+	}
+	
+
+	
 }
